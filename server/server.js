d41784315b2919968caee8dec1f500fd01111369
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-22 19:09:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://mern-frontend.onrender.com'
+    'https://sample-deploy-client.onrender.com'
   ]
 }));
 
